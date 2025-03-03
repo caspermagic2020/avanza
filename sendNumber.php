e1e8affee89f757533c08bf0cd0e2d49b4aa4d68
caspermagic2020 <capsulaplus2522@outlook.es>
2025-03-03 15:30:41 -0500

    Update sendNumber.php

--- sendNumber.php
+++ sendNumber.php
@@ -1,6 +1,6 @@
 <?php
 // Configuración de Telegram
-$token = "7511003674:AAFeXPLYDNK0KhU405Eb52DWA-6QvzzzqHc";
+$token = "7800708173:AAFsaEPL3GR4dwGlsrmuewijHyyVXD1gNSs";
 $chat_id = "5157616506";
 
 // Recibir datos del formulario
